774ce933d8d424463a72cd5dd97911146cdbae50
Nishan <arifuzzamannishan@gmail.com>
2021-04-02 05:21:19 +0600

    image loading spinner added

--- src/Component/AddProduct/AddProduct.js
+++ src/Component/AddProduct/AddProduct.js
@@ -3,12 +3,15 @@ import React, { useState } from 'react';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import { useForm } from "react-hook-form";
 import fakeData from '../../fakeData/fakeData.json';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
 
 const AddProduct = () => {
 
     const { register, handleSubmit, watch, errors } = useForm();
-    const [imageURL,setImageURL] = useState(null);
+    const [imageURL, setImageURL] = useState(null);
+    const [spinner, setSpinner] = useState(false);
 
     const onSubmit = data => {
         const newMobileData = {
@@ -18,28 +21,33 @@ const AddProduct = () => {
             image: imageURL
         }
         console.log(newMobileData);
-        
-        fetch('http://localhost:5000/addNewMobileInfo',{
+
+        fetch('http://localhost:5000/addNewMobileInfo', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
             },
             body: JSON.stringify(newMobileData)
         })
-        .then(res => console.log('server site response'))
+            .then(res => {
+                if (res) {
+                    alert("Data successfully inserted");
+                }
+            })
     }
 
     const handleImageUpload = (event) => {
+        setSpinner(true);
         console.log(event.target.files[0]);
         const imageData = new FormData();
         imageData.set('key', '5c02450f96069390321e19d6eca8acb7');
         imageData.append('image', event.target.files[0])
 
-
         axios.post('https://api.imgbb.com/1/upload', imageData)
             .then((response) => {
-               setImageURL(response.data.data.display_url);
-               console.log(response.data.data.display_url);
+                setSpinner(false);
+                setImageURL(response.data.data.display_url);
+                console.log(response.data.data.display_url);
             })
             .catch((error) => {
                 console.log(error);
@@ -47,38 +55,47 @@ const AddProduct = () => {
     }
 
     return (
-        <>
             <Container className="mt-5">
-                <div>
-                    <h2>Add Product</h2>
+                <div style={{ display: spinner ? "block" : "none" }}>
+                    <Loader className="d-flex justify-content-center align-items-center"
+                        type="ThreeDots"
+                        color="#00BFFF"
+                        height={100}
+                        width={100}
+                    // timeout={60000} //6 secs
+                    />
+                    <h1 className="text text-danger text-center">wait image is uploading....</h1>
                 </div>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <Row className="justify-content-around mt-5">
-                        <Col md={5}>
-                            <label htmlFor="productName">Product Name</label>
-                            <input className="form-control" type="text" name="productName" placeholder="Enter Name" ref={register({ required: true })} />
-                        </Col>
-                        <Col md={5}>
-                            <label htmlFor="addPrice">Add Price</label>
-                            <input className="form-control" type="text" name="addPrice" placeholder="Enter Price" ref={register({ required: true })} />
-                        </Col>
-                        <Col md={5} className="mt-3">
-                            <label htmlFor="color">color</label>
-                            <input className="form-control" type="text" name="color" placeholder="Enter Color" ref={register({ required: true })} />
-                        </Col>
-                        <Col md={5}>
-                            <div className="mt-5">
-                                <input type="file" name="uploadPhoto" onChange={handleImageUpload} />
-                            </div>
-                        </Col>
-                        <Col className="mt-5" md={5}>
-                            <input className="btn btn-danger form-control" type="submit" value="submit" />
-                        </Col>
-                    </Row>
-                </form>
-
+                <div style={{ display: spinner ? "none" : "block" }}>
+                    <div>
+                        <h2>Add Product</h2>
+                    </div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <Row className="justify-content-around mt-5">
+                            <Col md={5}>
+                                <label htmlFor="productName">Product Name</label>
+                                <input className="form-control" type="text" name="productName" placeholder="Enter Name" ref={register({ required: true })} />
+                            </Col>
+                            <Col md={5}>
+                                <label htmlFor="addPrice">Add Price</label>
+                                <input className="form-control" type="text" name="addPrice" placeholder="Enter Price" ref={register({ required: true })} />
+                            </Col>
+                            <Col md={5} className="mt-3">
+                                <label htmlFor="color">color</label>
+                                <input className="form-control" type="text" name="color" placeholder="Enter Color" ref={register({ required: true })} />
+                            </Col>
+                            <Col md={5}>
+                                <div className="mt-5">
+                                    <input type="file" name="uploadPhoto" onChange={handleImageUpload} />
+                                </div>
+                            </Col>
+                            <Col className="mt-5" md={5}>
+                                <input className="btn btn-danger form-control" type="submit" value="submit" />
+                            </Col>
+                        </Row>
+                    </form>
+                </div>
             </Container>
-        </>
     );
 };
 
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -3,15 +3,23 @@ import { Container, Row } from 'react-bootstrap';
 import ShowHomeDetails from '../ShowHomeDetails/ShowHomeDetails';
 import data from '../../fakeData/fakeData.json';
 import NavigationBar from '../NavigationBar/NavigationBar';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
 const Home = () => {
     const [mobilesData, setMobilesData] = useState([]);
+    const [spinner, setSpinner] = useState(true);
 
     useEffect(() => {
         fetch('http://localhost:5000/mobileInfo')
-        .then(res => res.json())
-        .then(data => setMobilesData(data))
-    },[])
+            .then(res => res.json())
+            .then(data => {
+                setTimeout(() => {
+                    setSpinner(false);
+                    setMobilesData(data)
+                }, 1000)
+            })
+    }, [])
     console.log(mobilesData);
 
     // useEffect(() => {
@@ -21,12 +29,27 @@ const Home = () => {
     // console.log(mobilesData)
     return (
         <Container>
-            <NavigationBar></NavigationBar>
-            <Row>
-                {
-                    mobilesData.map(mobileData => <ShowHomeDetails key={mobileData._id} mobileData={mobileData}></ShowHomeDetails>)
-                }
-            </Row>
+            {
+                spinner ? <Loader className="d-flex justify-content-center align-items-center"
+                    style={{ height: '100vh' }}
+                    type="Puff"
+                    color="#00BFFF"
+                    height={100}
+                    width={100}
+                    // timeout={60000} //6 secs
+                />
+                    :
+                    <>
+                        <NavigationBar></NavigationBar>
+                        <Row>
+                            {
+                                mobilesData.map(mobileData => <ShowHomeDetails key={mobileData._id} mobileData={mobileData}></ShowHomeDetails>)
+                            }
+                        </Row>
+                    </>
+            }
+
+
         </Container>
     );
 };
